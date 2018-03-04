7b58572da288df6d6dc7f2d7795e5b0c9a380cbd
nguyennam <ad.gtmb@gmail.com>
2018-03-04 14:23:42 +0700

    rafactor update check on admin dashboard

--- hm_admin/dashboard.php
+++ hm_admin/dashboard.php
@@ -23,19 +23,19 @@ if (isset($_SESSION['check_update_session'])) {
     $check_update_session = $_SESSION['check_update_session'];
 }
 if ($check_update_session == FALSE) {
-    $check_url = HM_API_SERVER . '/api/update/check';
+    $check_url = HM_GIT_RAW . '/.version';
     @$content = file_get_contents($check_url);
     $content = json_decode($content, TRUE);
     if (is_array($content)) {
         $args['update_noti'] = '';
         if (ALLOW_UPDATE == TRUE) {
-            if ($content['newest'] == HM_VERSION) {
+            if (isset($content['newest']) AND $content['newest'] == HM_VERSION) {
                 $args['update_noti'] = '<div class="alert alert-success" role="alert">Bạn đang cài đặt phiên bản mới nhất</div>';
             } else {
                 $args['update_noti'] = '<div class="alert alert-danger" role="alert">Đã có phiên bản mới, bạn có thể cập nhật hoặc truy cập hoamaisoft.com để tải về</div>';
             }
         }
-        if ($content['newest'] != HM_VERSION) {
+        if (isset($content['newest']) AND $content['newest'] != HM_VERSION) {
             if (isset($content['required']) AND $content['required'] == '1') {
                 update_core();
             }
